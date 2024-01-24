8c00c1bdfaf13949935bb2dcb54a61f5e1d5c901
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-01-24 15:48:41 +0200

    Refactor code

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,19 @@
-const myLibrary = [];
+let myLibrary = [];
+const booksList = document.querySelector(".books");
+const item = document.createElement("span");
+item.className = "item";
+const title = document.createElement("div");
+title.className = "title";
+const author = document.createElement("div");
+author.className = "author";
+const pages = document.createElement("div");
+pages.className = "pages";
+const read = document.createElement("div");
+read.className = "read";
+const remove = document.createElement("div");
+const removeIcon = '<svg viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>';
+remove.className = "remove";
+remove.innerHTML = removeIcon;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -13,46 +28,18 @@ function Book(title, author, pages, read) {
   };
 }
 
-const aGameOfThrones = new Book("A Game of Thrones", "George R. R. Martin", 694, true);
-addBookToLibrary(aGameOfThrones);
-const aClashOfKings = new Book("A Clash of Kings", "George R. R. Martin", 708, false);
-addBookToLibrary(aClashOfKings);
-const aStormOfSwords = new Book("A Storm of Swords", "George R. R. Martin", 973, false);
-addBookToLibrary(aStormOfSwords);
+function addFirstBooks() {
+  const aGameOfThrones = ([new Book("A Game of Thrones", "George R. R. Martin", 694, true), remove]);
+  const aClashOfKings = ([new Book("A Clash of Kings", "George R. R. Martin", 708, false), remove]);
+  const aStormOfSwords = ([new Book("A Storm of Swords", "George R. R. Martin", 973, false), remove]);
+  const aFeastForCrows = ([new Book("A Feast For Crows", "George R. R. Martin", 753, false), remove]);
+  const aDanceWithDragons = ([new Book("A Dance With Dragons", "George R. R. Martin", 1016, false), remove]);
 
-function addBookToLibrary(book) {
-  const booksList = document.querySelector(".books");
-  myLibrary.push(book);
-  let index = myLibrary.indexOf((book));
-  const item = document.createElement("span");
-  item.className = "item";
-  const title = document.createElement("div");
-  title.className = "title";
-  const author = document.createElement("div");
-  author.className = "author";
-  const pages = document.createElement("div");
-  pages.className = "pages";
-  const read = document.createElement("div");
-  read.className = "read";
-  const remove = document.createElement("div");
-  const removeIcon = '<svg viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>';
-  remove.className = "remove";
-  item.appendChild(title);
-  title.textContent = myLibrary[index].title;
-  item.appendChild(author);
-  author.textContent = myLibrary[index].author;
-  item.appendChild(pages);
-  pages.textContent = myLibrary[index].pages;
-  item.appendChild(read);
-  read.textContent = myLibrary[index].read;
-  item.appendChild(remove);
-  remove.innerHTML = removeIcon;
-  booksList.appendChild(item);
-}
+  myLibrary.push(aGameOfThrones, aClashOfKings, aStormOfSwords, aFeastForCrows, aDanceWithDragons);
 
-/*function displayLibrary() {
-  const booksList = document.querySelector(".books");
   for (let i = 0; i < myLibrary.length; i++) {
+
+    const booksList = document.querySelector(".books");
     const item = document.createElement("span");
     item.className = "item";
     const title = document.createElement("div");
@@ -63,17 +50,33 @@ function addBookToLibrary(book) {
     pages.className = "pages";
     const read = document.createElement("div");
     read.className = "read";
+    const remove = document.createElement("div");
+    const removeIcon = '<svg viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>';
+    remove.className = "remove";
+    remove.innerHTML = removeIcon;
+    
+    title.textContent = myLibrary[i][0].title;
+    author.textContent = myLibrary[i][0].author;
+    pages.textContent = myLibrary[i][0].pages;
+    read.textContent = myLibrary[i][0].read;
     item.appendChild(title);
-    title.textContent = myLibrary[i].title;
     item.appendChild(author);
-    author.textContent = myLibrary[i].author;
     item.appendChild(pages);
-    pages.textContent = myLibrary[i].pages;
     item.appendChild(read);
-    read.textContent = myLibrary[i].read;
+    item.appendChild(remove);
+
+    remove.addEventListener("click", event => {
+      event.target.closest(".remove").parentNode.remove();
+      myLibrary = myLibrary.filter(v => v[1] !== event.target.closest(".remove"));
+      console.log("deleting: " + myLibrary[i][0].title);
+      //myLibrary.pop(i);
+    });
+
     booksList.appendChild(item);
   };
-}*/
+}
+
+addFirstBooks();
 
 const dialog = document.getElementById("dialog");
 const newButton = document.querySelector(".new");
@@ -89,20 +92,28 @@ addButton.addEventListener("click", () => {
   const authorInput = document.querySelector("#new-author").value;
   const pagesInput = document.querySelector("#new-pages").value;
   const readInput = document.querySelector("#new-read").value;
-  let newEntry = new Book(titleInput, authorInput, pagesInput, readInput);
-  addBookToLibrary(newEntry);
+
+  myLibrary.push([new Book(titleInput, authorInput, pagesInput, readInput), remove]);
+
+  remove.addEventListener("click", event => {
+    event.target.closest(".remove").parentNode.remove();
+    myLibrary = myLibrary.filter(v => v[1] !== event.target.closest(".remove"));
+  });
+
+  title.textContent = titleInput;
+  author.textContent = authorInput;
+  pages.textContent = pagesInput;
+  read.textContent = readInput;
+
+  item.appendChild(title);
+  item.appendChild(author);
+  item.appendChild(pages);
+  item.appendChild(read);
+  item.appendChild(remove);
+
+  booksList.appendChild(item);
 });
 
 closeButton.addEventListener("click", () => {
   dialog.close();
 });
-
-const removeButton = document.getElementsByClassName("remove");
-for (let i = 0 ; i < removeButton.length; i++) {
-  let removedBook = removeButton[i].parentNode;
-  removeButton[i].addEventListener("click", () => {
-    myLibrary.pop(i);
-    removedBook.remove();
-    console.log("removing book" + i)
-  }); 
-}
